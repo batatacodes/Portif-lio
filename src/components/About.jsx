093b7e1116e4d57f5653c0f415b96451fe9772ab
batatacodes <225508232+batatacodes@users.noreply.github.com>
2025-09-14 18:30:41 -0300

    Create About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About(){
+  return (
+    <section className="about card">
+      <h2>Sobre mim</h2>
+      <p>Eu gosto muito de programação. Gosto de criar projetos e explorar novas ideias. Me chame de <strong>Batata</strong> ou <strong>Batatas</strong>. Meu nome verdadeiro é <strong>Isllan</strong>.</p>
+    </section>
+  )
+}
